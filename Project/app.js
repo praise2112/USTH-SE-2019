7be0a99bfcca5e7869c32eb426c4cbb00b7641e6
Praise2112 <oketola.praise@gmail.com>
2019-03-25 15:07:19 +0700

    Add login and signUp funtionality

--- Project/app.js
+++ Project/app.js
@@ -1,18 +1,28 @@
-var express = require("express");
-var app = express();
-var http = require('http');
-var path = require('path');
-var bodyParser = require('body-parser');
-var flash = require('connect-flash');
-var mongoose = require('mongoose');
-var session = require('express-session');
+var express         = require("express"),
+    app             = express(),
+    http            = require('http'),
+    path            = require('path'),
+    bodyParser      = require('body-parser'),
+    flash           = require('connect-flash'),
+    mongoose        = require('mongoose'),
+    session         = require('express-session'),
+    passport        = require("passport"),  // login and SignUp authentication
+    LocalStrategy   = require("passport-local"),     // login and SignUp authentication
+    cookieParser    = require("cookie-parser"),
+    methodOverride  = require("method-override"),
+    User            = require("./models/user");     // user model
 
-//setup db
-mongoose.connect('mongodb://localhost:27017/projectdb', { useNewUrlParser: true });
+//cofigure dotenv
+var dotenv = require('dotenv').config({path: path.join(__dirname, '.env')}); // for env variables
 
 //router setup
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRoutes = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+//setup db
+mongoose.set("useCreateIndex", true);
+mongoose.connect('mongodb://localhost:27017/projectdb', { useNewUrlParser: true });
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,14 +30,39 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
-app.use(flash()); 
+app.use(flash());
+app.use(methodOverride("_method"));     // for authentication
+app.use(cookieParser('secret'));        // for authentication
+
+
+//  PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "One again Rusty wins the cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// making a middleware for all routes on the page to get current user, error or success
+// this are global variables(can be accessed from any file)
+app.use(async function (req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error =  req.flash("error");
+    res.locals.success =  req.flash("success");
+    next();
+});
+
+//router config
+app.use('/', indexRoutes);
+// app.use('/users', usersRouter);
 
 app.listen(3000, function(){
     console.log("Sever started at port 3000");
-})
+});
 
-//router config
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
--- Project/models/user.js
+++ Project/models/user.js
@@ -1,59 +1,56 @@
 var mongoose = require('mongoose');
+var passportLocalMongoose = require("passport-local-mongoose");
 var bcrypt = require('bcrypt');
-var Schema = mongoose.Schema;
+
 
 //Create table
-var Userschema = new Schema({
+var UserSchema = new mongoose.Schema({
 	//Data goes here
-	name : { 
-		type: String, 
-		required: [true, "can't be blank"]
+	username : {
+		type: String,
 	},
 	email : { 
 		type: String, 
-		required: [true, "can't be blank"],
+		required: true,
 		unique: true
 	},
 	password: { 
 		type: String, 
-		required: [true, "can't be blank"]
 	},
-	comfirmpw: { 
-		type: String, 
-		required: [true, "can't be blank"]
-	},
+	isAdmin: {type: Boolean, default: false},
+
 	favorites: [{ 
 		type: mongoose.Schema.Types.ObjectId, 
 		ref: 'pizza' 
 	}],
 });
 
-UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+// UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
 // Bcrypt password function when saving
-UserSchema.pre('save', function (next) {
-  var user = this;
-  bcrypt.hash(user.password, 10, function (err, hash){
-    if (err) {
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  })
-});
+// UserSchema.pre('save', function (next) {
+//   var user = this;
+//   bcrypt.hash(user.password, 10, function (err, hash){
+//     if (err) {
+//       return next(err);
+//     }
+//     user.password = hash;
+//     next();
+//   })
+// });
 
 //Pre-create favourite food function
-UserSchema.methods.favorite = function(id){
-  if(this.favorites.indexOf(id) === -1){
-    this.favorites.push(id);
-  }
+// UserSchema.methods.favorite = function(id){
+//   if(this.favorites.indexOf(id) === -1){
+//     this.favorites.push(id);
+//   }
+//   return this.save();
+// };
+//
+// UserSchema.methods.unfavorite = function(id){
+//   this.favorites.remove(id);
+//   return this.save();
+// };
 
-  return this.save();
-};
-
-UserSchema.methods.unfavorite = function(id){
-  this.favorites.remove(id);
-  return this.save();
-};
-
-module.export = mongoose.model('User',Userschema);
+UserSchema.plugin(passportLocalMongoose);	// using passport validation
+module.exports = mongoose.model('User',UserSchema);
